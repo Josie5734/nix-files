be754c0119d28de78cc79d060829c0478352cf79
Josie5734 <armyjoe999@gmail.com>
2026-07-20 04:28:09 +0100

    updated mako module

--- modules/sway/mako.nix
+++ modules/sway/mako.nix
@@ -5,6 +5,12 @@
     enable = true;
     settings = {
       font = "JetBrains Mono Nerd";
+      default-timeout = 5000;
+      # catppuccin colors
+      text-color = "#cdd6f4ff";
+      background-color = "#1e1e2eff";
+      border-color = "#cba6f7ff";
+      border-radius = 10;
     };
   };
 }
